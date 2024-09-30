88db0108feae09b30a6e137ea5f83ec0abcc32c1
matilda002 <matilda0817@hotmail.com>
2024-09-30 14:25:48 +0200

    added database and npqsql connection

---
+++ DatabaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+
+public class DatabaseConnection
+{
+    private readonly string _connectionString;
+
+    public DatabaseConnection()
+    {
+        _connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=ravens;";
+    }
+
+    public NpgsqlConnection GetConnection()
+    {
+        return new NpgsqlConnection(_connectionString);
+    }
+}
