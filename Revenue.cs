ec40d4d8b689fdc0116364d7234a3614f191216a
matilda002 <matilda0817@hotmail.com>
2024-09-30 15:03:04 +0200

    added method to get currentdate revenue

--- Revenue.cs
+++ Revenue.cs
@@ -1,31 +1,41 @@
 namespace Ravens;
+
 using Npgsql;
 
-public class FinanceRevenue {
-    
-    public void GetRevenue(){
+public class FinanceRevenue
+{
+    private object? ConnectionExecuteScalar(string query)
+    {
+        try
+        {
+            using var connection = new DatabaseConnection().GetConnection();
+            connection.Open();
 
-        using var connection = new DatabaseConnection().GetConnection();
-        connection.Open();
+            using var cmd = new NpgsqlCommand(query, connection);
+            return cmd.ExecuteScalar();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return null;
+        }
+    }
 
+    public void GetRevenue()
+    {
         const string qSelectRevenue = "select sum(revenue) from finance";
-        
-        using var cmd = new NpgsqlCommand(qSelectRevenue, connection);
-        var totalRevenue = cmd.ExecuteScalar();
-        
+
+        var totalRevenue = ConnectionExecuteScalar(qSelectRevenue);
+
         Console.WriteLine($"Totala summan intäkter: {totalRevenue} kr");
     }
 
-    public void GetTodaysRevenue(){
-        
-        using var connection = new DatabaseConnection().GetConnection();
-        connection.Open();
+    public void GetCurrentDateRevenue()
+    {
+        const string qSelectCurrenDateRevenue = "select sum(revenue) from finance where datetime = current_date";
 
-        const string qSelectTodaysRevenue = "select sum(revenue) from finance where datetime = current_date";
-        
-        using var cmd = new NpgsqlCommand(qSelectTodaysRevenue, connection);
-        var TodaysRevenue = cmd.ExecuteScalar();
-        
-        Console.WriteLine($"Dagens intäkter: {TodaysRevenue} kr");
+        var currentDateRevenue = ConnectionExecuteScalar(qSelectCurrenDateRevenue);
+
+        Console.WriteLine($"Dagens intäkter: {currentDateRevenue} kr");
     }
 }
