d3df4512569a413903a472503935f344ed0725cc
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2024-10-21 10:30:13 +0200

    changed spacing

--- Program.cs
+++ Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         var financeMethods = new FinanceMethods();
-
-        Console.WriteLine("\nTesting GetUtgifter method:");
+        
+        Console.WriteLine("\n");
         financeMethods.GetUtgifter();
 
-        Console.WriteLine("\nTesting GetAllUtgifter method:");
+        Console.WriteLine("\n");
         var utgifter = financeMethods.GetAllUtgifter();
 
-        Console.WriteLine($"{"Titel",-30}{"Kund ID",-10}{"Utgifter",-15}{"Datum",-15}");
+        Console.WriteLine($"{"Titel",-25}{"Kund ID",-12}{"Utgifter",-15}{"Datum",-15}");
         Console.WriteLine(new string('-', 70));
 
         foreach (var utgift in utgifter)
         {
-            Console.WriteLine($"{utgift.Title,-30}{utgift.CustomerId,-10}" +
+            Console.WriteLine($"{utgift.Title,-25}{utgift.CustomerId,-12}" +
                               $"{utgift.Expenditure,-15}{utgift.Date.ToShortDateString(),-15}");
         }
     }
